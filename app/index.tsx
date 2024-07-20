293a23f5849ef219ca6ed239d0933b1b127780d5
ekele <ekele19stephen96u@gmail.com>
2024-07-21 00:13:37 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="bg-amber-600 flex items-center justify-center h-full">
+            <Text>Select uploadthingImage</Text>
+        </View>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ...
+};
